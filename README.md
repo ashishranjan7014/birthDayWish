0248131b0753da22eaa3fa74e9b347361b489c2f
ashishranjan7014 <ashishranjan7014@gmail.com>
2022-06-12 13:53:37 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,72 @@
+# Happy Birthday Wisher
+
+This is a simple [Next.js](https://nextjs.org/) project where you can generate a birthday wish for someone.
+
+Just enter their name, press Go and share them the generated link :)
+
+Don't forget to ⭐️ this repository!
+
+## Local Setup
+
+First, clone this repository:
+
+```bash
+git clone https://github.com/ashishranjan7014/birthDayWish
+```
+
+Install the dependencies:
+
+```bash
+npm install
+```
+
+Then, run the development server:
+
+```bash
+npm run dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
+
+## Contributing
+In order to make a hassle-free environment, please make sure to read the [Contribution Guidelines](CONTRIBUTING.md) and follow all the steps mentioned there.
+Are we missing any of your favorite features, which you think you can add to it❓ Feel free to do so. <br />
+
+All contributions are welcome.
+
+## Learn More
+
+To learn more about Next.js, take a look at the following resources:
+
+- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
+- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+
+You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
+
+## License
+
+```
+MIT License
+
+Copyright (c) 2021 Gourav Khunger
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+```
---
+++ pages/[...name].js
@@ -0,0 +1,139 @@
+import React, { useState, useEffect, useRef } from "react";
+import Head from "next/head";
+import styles from "../styles/Name.module.css";
+import { useRouter } from "next/router";
+import ConfettiGenerator from "confetti-js";
+import messages from "../utils/birthdayWishes.js";
+import useTheme from "../hooks/useTheme";
+import Theme from "../components/Theme/Theme";
+import ImageFader from "../components/ImageFader/ImageFader";
+
+const Wish = ({ }) => {
+  const router = useRouter();
+  const { name } = router.query; // gets both name & color id in form of array [name,colorId]
+  const color = name ? name[1] : 4; //extracting colorId from name
+  const [isDarkMode, setIsDarkMode] = useState(true);
+
+
+  const audioRef = useRef();
+  const { themes, setTheme, currentTheme } = useTheme();
+
+  useEffect(() => {
+    // Theme Change
+    setTheme(color);
+    let confetti;
+    // Confetti
+    const confettiSettings = {
+      target: "canvas",
+      start_from_edge: true,
+      rotate: true,
+      clock: 250,
+      size: 4,
+      max: 80
+    };
+    confetti = new ConfettiGenerator(confettiSettings);
+    confetti.render();
+    return () => confetti.clear();
+  }, [color]);
+
+  // function for randomly picking the message from messages array
+  const randomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min)) + min;
+  };
+
+  const title = (name) => {
+    const wish = "Happy Birthday " + name + "!";
+    const base_letters = [];
+    const name_letters = [];
+
+    for (let i = 0; i < wish.length; i++) {
+      if (i < 15) {
+        const letter = wish.charAt(i);
+        base_letters.push(
+          <span key={i} style={{ "--i": i + 1 }}>
+            {letter}
+          </span>
+        );
+      } else {
+        const letter = wish.charAt(i);
+        name_letters.push(
+          <span key={i} style={{ "--i": i + 1 }} className={styles.span}>
+            {letter}
+          </span>
+        );
+      }
+    }
+
+    return (
+      <h1 className={styles.title} style={{ "--wish-length": wish.length }}>
+        <div>{base_letters.map((letter) => letter)}</div>
+        <div className={styles.dynamicWord}>{name_letters.map((letter) => letter)}</div>
+      </h1>
+    );
+  };
+
+  return (
+    <div className={`${isDarkMode ? styles.dark_mode : styles.light_mode} ${styles.container}`}>
+      <Head>
+        <title>Happy Birthday {name && name[0]}</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link href='https://fonts.googleapis.com/css?family=Sacramento:400' rel='stylesheet' type='text/css' />
+      </Head>
+
+      <canvas className={styles.canvas}
+        style={{ backgroundColor: "transparent", left: 0, top: 0 }}
+        id="canvas"></canvas>
+
+      <main className={styles.animate}>
+        <div className={styles.Main_Wapper}>
+          <div className={styles.Wapper_Left} style={{ backgroundColor: isDarkMode ? "black" : "white" }}>
+            <div id="bgi">
+              <img id="gifBorder" src="/media/bgi1.gif" width={"100%"} height={"100%"} />
+            </div>
+          </div>
+          <div className={styles.Wapper_Right} style={{ backgroundColor: isDarkMode ? "black" : "white" }}>
+            <div className={styles.contentWrapper}>
+              <ImageFader />
+              <Theme />
+              <div className={styles.wrapper}>
+                <div className={styles.checkboxWrapper}>
+                  <input type="checkbox" className={styles.checkbox} checked={isDarkMode} id="checkbox" onChange={() => { setIsDarkMode(!isDarkMode) }} />
+                  <label htmlFor="checkbox" className={styles.label} style={{ backgroundColor: !isDarkMode ? "black" : "white" }}>
+                    <i className={`${styles.fa_moon}`}></i>
+                    <i className={`${styles.fa_sun}`}></i>
+                    <div className={styles.ball} style={{ backgroundColor: isDarkMode ? "black" : "white" }}></div>
+                  </label>
+                </div>
+                <div className={styles.main}>
+                  {title(name && name[0] && name[0])}
+                  <p className={styles.desc}>
+                    {messages[randomNumber(0, messages.length)].value}
+                  </p>
+                </div>
+                <div className={styles.imageWrapper}>
+                  <img src="media/cake.gif" />
+                  <img src="media/cake.gif" />
+                  <img src="media/cake.gif" />
+                </div>
+              </div>
+              <div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.contentWrapper}>
+          <div id="image-container"></div>
+        </div>
+      </main>
+      <audio ref={audioRef} id="player" autoPlay loop >
+        <source src="media/happy-birthday-to-you.mp3" />
+        <source src="media/happy-birthday-to-you-electronic.mp3" />
+        <source src="media/happy-birthday-to-you-upbeat.mp3" />
+        <source src="media/happy-birthday-to-you-baby.mp3" />
+      </audio>
+    </div>
+  );
+};
+
+export default Wish;
